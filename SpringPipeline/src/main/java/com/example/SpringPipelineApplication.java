67b919cc13b7951d4e7de40d61bec4760706a3ac
Abhishek <Abhishek_Kumar@intuit.com>
2019-08-31 00:05:54 +0530

    Abhi

---
+++ SpringPipeline/src/main/java/com/example/SpringPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPipelineApplication.class, args);
+	}
+
+}
